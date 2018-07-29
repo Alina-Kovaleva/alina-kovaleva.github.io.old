d5df919f7872bb766a03dd11f46e1ed7857c4796
Alina Kovaleva <a.d.kovaleva@yandex.com>
2018-07-29 20:53:24 +0200

    Update random-quote-machine-script.js
    changed all js and fixed API problem

--- js/random-quote-machine-script.js
+++ js/random-quote-machine-script.js
@@ -1,46 +1,15 @@
-
-
-//function newQuote(){
-//  $.getJSON( "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=xml&lang=en", function( data ) {
-//    var text = data.quoteText;
-//    var author = data.quoteAuthor;
-//    if (author === '') {
-//      author = 'Author unknown';
-//    }
-//    $(".quote").hide().html('<p class="content"><em>"' + text +'"</em></p>' + ' <br><p class="author"> ' + author + ' </p> ').fadeIn('slow');
-//    $('.tweet-quote').attr('href', 'https://twitter.com/intent/tweet?text=' + '"' + text + '"' + ' - ' + author);
-//  });
-//}
-//$(document).ready(function(){
-//    getQuote();
-//    $(".new-quote").on("click", getQuote());
-//});
-
-
-
-
+function newQuote(){
+  $.getJSON( "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function( data ) {
+    var text = data.quoteText;
+    var author = data.quoteAuthor;
+    if (author === '') {
+      author = 'Author unknown';
+    }
+    $(".quote").hide().html('<p class="content"><em>"' + text +'"</em></p>' + ' <br><p class="author"> ' + author + ' </p> ').fadeIn('slow');
+    $('.tweet-quote').attr('href', 'https://twitter.com/intent/tweet?text=' + '"' + text + '"' + ' - ' + author);
+  });
+}
 $(document).ready(function(){
-    var getQuote = function(){
-        return $.ajax({
-            url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
-            headers: {
-              "X-Mashape-Key": "frU2BZvBvXmshf9FIhGdY8SGKNjDp1MecBEjsn7XVOAR8Knobr"  
-            },
-            method: "POST",
-            contentType: "application/x-www-form-urlencoded",
-            dataType: 'json',
-        })
-    };
-    
-    var setQuote = function(data){
-        $(".quote").hide().html('<p class="content"><em>"' + data.quote +'"</em></p>' + ' <br><p class="author"> ' + data.author + ' </p> ').fadeIn('slow');
-        $('.tweet-quote').attr('href', 'https://twitter.com/intent/tweet?text=' + '"' + data.quote + '"' + ' - ' + data.author);        
-    };
-    
-    	$('.new-quote').on('click', function() {
-		  var reloadBtn = $(this);
-		  getQuote().done(function(data) {
-              setQuote(data);
-		});
-	}); 
-})
+  newQuote();
+  $(".new-quote").on("click", newQuote);
+});
